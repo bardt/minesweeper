17b4277e1e747e8220849752f676bd107484f363
Roman Salnikov <roman@bardt.me>
2016-10-14 17:15:18 +0700

    Add initial map

--- Main.elm
+++ Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Html exposing (Html, text, div, h1)
 import Html.App as HtmlApp
+import Matrix exposing (Matrix)
 
 
 main : Program Never
@@ -14,12 +15,50 @@ main =
 
 
 type alias Model =
-    {}
+    { map : Map
+    }
+
+
+type alias Map =
+    Matrix Square
+
+
+type alias Square =
+    { hasMine : Bool
+    , isCovered : Bool
+    }
 
 
 initialModel : Model
 initialModel =
-    {}
+    { map = initialMap
+    }
+
+
+initialMap : Map
+initialMap =
+    let
+        -- Short for Free
+        f =
+            { hasMine = False
+            , isCovered = True
+            }
+
+        -- Short for Mine
+        m =
+            { hasMine = True
+            , isCovered = True
+            }
+
+        x =
+            [ [ f, m, f, f, f ]
+            , [ m, f, f, f, m ]
+            , [ f, f, f, f, f ]
+            , [ m, f, m, f, f ]
+            , [ f, f, f, f, f ]
+            ]
+    in
+        Matrix.fromList x
 
 
 type Msg
